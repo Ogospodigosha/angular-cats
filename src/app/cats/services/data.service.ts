2645c1f2655790b37f05f6f8b5926d51614e1d58
Ogospodigosha <odintsovis@hotmail.com>
2023-12-15 14:27:09 +0300

    rewrite getCats

--- src/app/cats/services/data.service.ts
+++ src/app/cats/services/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import {BehaviorSubject, catchError, scan} from "rxjs";
+import {BehaviorSubject, catchError, scan, tap} from "rxjs";
 import { environment } from "../../../environments/environments";
 import { CatsFacts, CatsFactsResponse } from "../models/cats.models";
 import { CatchErrorService } from "./catch-error.service";
@@ -16,13 +16,14 @@ export class DataService {
   getCats(page: number) {
     return this.http.get<CatsFactsResponse>(`${environment.baseUrl}?page=${page}`)
       .pipe(
-        catchError((error: HttpErrorResponse) => this.catchErrorService.handleHttpError(error))
-
-      )
-      .subscribe((res) => {
-        this.originalCatsData.next([...this.originalCatsData.value, ...res.data]);
-      });
+        catchError((error: HttpErrorResponse) => this.catchErrorService.handleHttpError(error)),
+        tap((res: CatsFactsResponse) => {
+          const currentData = this.originalCatsData.value;
+          this.originalCatsData.next([...currentData, ...res.data]);
+        })
+      ).subscribe(res =>{});
   }
+
 
   getOriginalCatsData() {
     return this.originalCatsData.asObservable();
