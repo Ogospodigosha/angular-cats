3d628b1c9c773d92743db72f915b6db1293eee93
Ogospodigosha <odintsovis@hotmail.com>
2023-12-17 21:19:56 +0300

    change search component

--- src/app/cats/components/search/search.component.ts
+++ src/app/cats/components/search/search.component.ts
@@ -1,18 +1,31 @@
-import {Component, EventEmitter,  Output} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent  {
+export class SearchComponent implements OnInit {
   searchText: string = '';
   @Output() searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
 
   constructor(private router: Router, private route: ActivatedRoute) {}
-  onSearchTextChanged(text: string) {
-    this.searchTextChanged.emit(text);
+
+  ngOnInit() {
+    const savedSearchText = this.route.snapshot.queryParams['searchText'] || localStorage.getItem('searchText');
+    if (savedSearchText) {
+      this.searchText = savedSearchText;
+      this.emitSearchText();
+    }
+  }
+
+  onSearchTextChanged() {
+    localStorage.setItem('searchText',this.searchText);
+    this.emitSearchText();
+  }
+
+  private emitSearchText() {
+    this.searchTextChanged.emit(this.searchText);
   }
 }
